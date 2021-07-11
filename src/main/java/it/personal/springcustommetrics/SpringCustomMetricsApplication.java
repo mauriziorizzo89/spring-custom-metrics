500a7fc932cfda9b797c35dc17eabdcfe8c6f236
Rizzo Maurizio <maurizio.rizzo@sisal.it>
2021-07-11 12:16:12 +0200

    first commit

---
+++ src/main/java/it/personal/springcustommetrics/SpringCustomMetricsApplication.java
@@ -0,0 +1,13 @@
+package it.personal.springcustommetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCustomMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCustomMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/personal/springcustommetrics/SpringCustomMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.personal.springcustommetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCustomMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
